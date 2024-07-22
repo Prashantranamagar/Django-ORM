8ec5dedae3f04475524b2eba4acaebd2d1eb95d8
Prashantranamagar <ranamagar.prashant@gmail.com>
2024-07-22 21:59:28 +0545

    command management

---
+++ practice_orm/management/commands/runorm.py
@@ -0,0 +1,17 @@
+# practice_orm/management/commands/run_orm_practice.py
+from django.core.management.base import BaseCommand
+from practice_orm.practice_orm import practice_orm
+
+
+class Command(BaseCommand):
+    help = 'Run ORM practice commands'
+
+    def handle(self, *args, **kwargs):
+        practice_orm()
+        
+   
+
+
+
+
+
